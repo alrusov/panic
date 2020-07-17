71c9d24c17d4d36b65698ba5a9bc6398363221b8
Alexey Rusov <rolic402@mail.ru>
2020-07-17 18:14:30 +0300

    +GetStack*

--- panic.go
+++ panic.go
@@ -4,6 +4,8 @@ Package panic implements a error handle
 package panic
 
 import (
+	"bytes"
+	"fmt"
 	"strings"
 
 	"github.com/alrusov/misc"
@@ -38,27 +40,37 @@ func SaveStackToLog() {
 	if enabled {
 		r := recover()
 		if r != nil {
-			misc.Logger("", "EM", "%v", r)
-
-			stack := misc.GetCallStack(1)
-			n := len(stack)
-			show := false
-			for i := 0; i < n; i++ {
-				df := stack[i]
-
-				if !show && !strings.HasPrefix(df.FuncName, "runtime.") {
-					show = true
-				}
-
-				if show {
-					misc.Logger("", "EM", ` at [%d] %s %s:%d`, n-i-1, df.FuncName, df.FileName, df.Line)
-				}
-			}
-
+			misc.Logger("", "EM", "%v\n%s", r, GetStack())
 			misc.StopApp(misc.ExPanic)
 			misc.Exit()
 		}
 	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+// GetStack -
+func GetStack() string {
+	stack := misc.GetCallStack(1)
+	n := len(stack)
+	buf := new(bytes.Buffer)
+	show := false
+	lf := ""
+
+	for i := 0; i < n; i++ {
+		df := stack[i]
+
+		if !show && !strings.HasPrefix(df.FuncName, "runtime.") {
+			show = true
+		}
+
+		if show {
+			buf.WriteString(fmt.Sprintf(`%s  at [%d] %s %s:%d`, lf, n-i-1, df.FuncName, df.FileName, df.Line))
+			lf = "\n"
+		}
+	}
+
+	return string(buf.Bytes())
 }
 
 //----------------------------------------------------------------------------------------------------------------------------//
